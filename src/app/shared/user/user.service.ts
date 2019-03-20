ad44ec213be6099b35d24e6b6e9394506c0c5a27
Mateusz Proskura <matpro29@outlook.com>
2019-03-20 20:24:29 +0100

    Hashowanie hasla

--- src/app/shared/user/user.service.ts
+++ src/app/shared/user/user.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +8,11 @@ import { Observable } from 'rxjs';
 
 export class UserService {
   public API = '//localhost:8080';
-  public USER_API = this.API + '/users';
+  public REGISTER_API = this.API + '/register';
 
   constructor(private http: HttpClient) { }
 
   save(user: any): Observable<any> {
-    let result: Observable<Object>;
-    if (user['href']) {
-      result = this.http.put(user.href, user);
-    } else {
-      result = this.http.post(this.USER_API, user);
-    }
-    return result;
+    return this.http.post(this.REGISTER_API, user);
   }
 }
